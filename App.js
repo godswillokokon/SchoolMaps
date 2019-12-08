c259be912e064cefd151be9b9751c2ae632032fb
godswillokokon <godswillokokon3@gmail.com>
2019-12-08 16:51:11 +0100

    landing screen done

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
+import { ApplicationProvider, Button, Icon, Layout, Text, IconRegistry, } from '@ui-kitten/components';
 import { mapping, light as lightTheme } from '@eva-design/eva';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
 
+
+
+export const FacebookIcon = (style) => (
+  <Icon {...style} name='google' pack='material' />
+);
 const HomeScreen = () => (
   <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text catetory='h1'>HOME</Text>
+    <Text catetory='h1'>Welcome to CRUTECH Maps</Text>
+    <Button> Login with Gmail</Button>
   </Layout>
 );
 
 const App = () => (
-  <ApplicationProvider mapping={mapping} theme={lightTheme}>
-    <HomeScreen />
-  </ApplicationProvider>
+  <React.Fragment>
+    <IconRegistry icons={EvaIconsPack} />
+    <ApplicationProvider mapping={mapping} theme={lightTheme}>
+      <HomeScreen />
+    </ApplicationProvider>
+  </React.Fragment>
 );
 
 export default App;
