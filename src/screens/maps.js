8b681798c85877419041b0e649ca18a537f93456
godswillokokon <godswillokokon3@gmail.com>
2020-01-28 15:47:11 +0100

    added markers about to release apk

--- src/screens/maps.js
+++ src/screens/maps.js
@@ -51,7 +51,213 @@ export const MapsScreen = ({ navigation }) => {
         }}
         mapType="satellite"
         showsUserLocation={true}
-      />
+
+      >
+        <Marker
+          coordinate={{
+            latitude: 4.9256721,
+            longitude: 8.3299248
+          }}
+          title="New  Science  Block  2"
+          description="LECTURE HALL"
+
+        />
+
+        <Marker
+          coordinate={{
+            latitude: 4.9256721,
+            longitude: 8.3299248
+          }}
+          title="Crutech Male Hostel"
+          description="HOSTEL"
+
+        />
+
+        <Marker
+          coordinate={{
+            latitude: 4.9260745,
+            longitude: 8.3293955
+          }}
+          title="Urban and regional Planning Studio"
+          description="STUDIO"
+
+        />
+
+
+        <Marker
+          coordinate={{
+            latitude: 4.9266998,
+            longitude: 8.3285267
+          }}
+          title="Mass Communication, Architecture Department"
+          description="Department"
+
+        />
+
+        <Marker
+          coordinate={{
+            latitude: 4.9267021,
+            longitude: 8.3289007
+          }}
+          title="Post Graduate School"
+          description="Post Graduate"
+
+        />
+        <Marker
+          coordinate={{
+            latitude: 4.9277183,
+            longitude: 8.3296246,
+          }}
+          title="CBT Center"
+          description="CBT Center"
+
+        />
+
+        <Marker
+          coordinate={{
+            latitude: 4.9280179,
+            longitude: 8.3295076,
+          }}
+          title="Old Computer Science Block"
+          description="Old Computer Science"
+
+        />
+
+        <Marker
+          coordinate={{
+            latitude: 4.9280179,
+            longitude: 8.3295076,
+          }}
+          title="Faculty of Architecture"
+          description="Faculty of Architecture"
+
+        />
+
+        <Marker
+          coordinate={{
+            latitude: 4.9280416,
+            longitude: 8.3308589,
+          }}
+          title="Hall 3 Crutech"
+          description="FEMALE HOSTEL"
+
+        />
+
+
+        <Marker
+          coordinate={{
+            latitude: 4.9280442,
+            longitude: 8.3311166,
+          }}
+          title="Visual Arts Department"
+          description="Department"
+
+        />
+
+        <Marker
+          coordinate={{
+            latitude: 4.9287231,
+            longitude: 8.3308663,
+          }}
+          title="Hall 2 Crutech"
+          description="FEMALE HOSTEL"
+
+        />
+
+        <Marker
+          coordinate={{
+            latitude: 4.9295288,
+            longitude: 8.3309286,
+          }}
+          title="Hall 1 Crutech"
+          description="FEMALE HOSTEL"
+
+        />
+
+        <Marker
+          coordinate={{
+            latitude: 4.9295315,
+            longitude: 8.3311551,
+          }}
+          title="Crutech SUG Solemn Assembly"
+          description="Solemn Assembly"
+
+        />
+
+        <Marker
+          coordinate={{
+            latitude: 4.9295315,
+            longitude: 8.3311551,
+          }}
+          title="General Store"
+          description="General Store"
+
+        />
+
+        <Marker
+          coordinate={{
+            latitude: 4.9295315,
+            longitude: 8.3311551,
+          }}
+          title="Engineering Old Faculty And Dean's Office"
+          description="Faculty"
+
+        />
+
+        <Marker
+          coordinate={{
+            latitude: 4.9284812,
+            longitude: 8.3298714,
+          }}
+          title="Old Faculty of Physical Science"
+          description="Faculty"
+
+        />
+
+        <Marker
+          coordinate={{
+            latitude: 4.9272299,
+            longitude: 8.3283398,
+          }}
+          title="ELECTRICAL ENGINEERING"
+          description="Faculty"
+
+        />
+
+        <Marker
+          coordinate={{
+            latitude: 4.9278726,
+            longitude: 8.328493,
+          }}
+          title="CIVIL ENGINEERING WORKSHOP"
+          description="Faculty"
+
+        />
+
+        <Marker
+          coordinate={{
+            latitude: 4.9296921,
+            longitude: 8.3290337,
+          }}
+          title="Library"
+          description="Library"
+
+        />
+
+        <Marker
+          coordinate={{
+            latitude: 4.9309294,
+            longitude: 8.3286361,
+          }}
+          title="Information Unit"
+          description="Information Unit"
+
+        />
+
+      </MapView>
+
+
+
     </SafeAreaView>
   );
 };
